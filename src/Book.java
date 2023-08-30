278f5f09c6486e214964e9445566b120c0d2d997
Darren <dchance1@mail.valenciacollege.edu>
2023-08-29 20:29:29 -0400

    Added multi-line class comment to Book.java to describe use.

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,15 @@
+/**
+ * Darren Chance<br>
+ * CEN 3024 - Software Development 1<br>
+ * August 28, 2023<br>
+ * Book.java<br>
+ * <p>
+ * The {@code Database} class represents a library database.
+ * <p>
+ * The class {@code Book} creates a book and stores the book's details. This allows for easy retrival of details such as
+ * title, book id number and author. This class does not contain any methods asside from getters and setters.
+ */
+
 public class Book {
     private String title;
     private int id;
